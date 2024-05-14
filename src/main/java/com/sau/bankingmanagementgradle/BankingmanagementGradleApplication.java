5abc27479c613473c68efc5ab6ff810cd3b0cd9a
ozangulll <ozan.gul1@ogr.sakarya.edu.tr>
2024-05-14 11:07:32 +0300

    Gradle changes (dependencies added.)

---
+++ src/main/java/com/sau/bankingmanagementgradle/BankingmanagementGradleApplication.java
@@ -0,0 +1,13 @@
+package com.sau.bankingmanagementgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingmanagementGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingmanagementGradleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sau/bankingmanagementgradle/BankingmanagementGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sau.bankingmanagementgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingmanagementGradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
